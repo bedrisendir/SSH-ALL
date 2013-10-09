f6654451ee7499674a207ccee856e404342e8315
Bedri Sendir <bsendir1@cs.binghamton.edu>
2013-10-09 17:30:45 -0400

    cassandra flush+json

--- src/SSHALL/SSHALL.java
+++ src/SSHALL/SSHALL.java
@@ -11,7 +11,14 @@ class SSHALL{
 	BufferedReader br = null;
 	System.out.println("Filename : "+args[0]);
 	System.out.println("Number of Nodes : "+args[1]);
+	System.out.println("Path For the Scripts : "+args[2]);
+	System.out.println("Output Path : "+args[3]);
+
+	String cassandra_path=args[2];
 	int num_of_nodes=Integer.valueOf(args[1]);
+	String output_path=args[3];
+	String cassandra_data_path=args[4];
+
 	ConcurrentLinkedQueue<String> nodes = new ConcurrentLinkedQueue <String>();
 	try{
 	    br = new BufferedReader(new FileReader(args[0]));
@@ -22,7 +29,7 @@ class SSHALL{
 	catch(IOException e){
 	
 	}
-	SSHWorker worker= new SSHWorker(nodes);
+	SSHWorker worker= new SSHWorker(nodes,cassandra_path,output_path,cassandra_data_path);
 	Thread[] t= new Thread[num_of_nodes];
 	for (int c = 0; c < num_of_nodes; c++) {
 	    t[c] = new Thread(worker);
--- src/SSHALL/SSHWorker.java
+++ src/SSHALL/SSHWorker.java
@@ -8,23 +8,33 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 class SSHWorker implements Runnable{
 
     ConcurrentLinkedQueue<String> nodes;
+    String cassandra_path;
+    String output_path;
+    String cassandra_data_path;
 
-    public SSHWorker(ConcurrentLinkedQueue<String> nodes){
+    public SSHWorker(ConcurrentLinkedQueue<String> nodes,String cassandra_path,String output_path,String cassandra_data_path){
         this.nodes=nodes;
+	this.cassandra_path=cassandra_path;
+	this.output_path=output_path;
+	this.cassandra_data_path=cassandra_data_path;
     }
 
     public void run(){
+	String nodename=nodes.poll();
         ProcessBuilder pb = new ProcessBuilder("ssh",
-                                               nodes.poll(),
-                                               "hostname");
-        pb.redirectErrorStream();
+                                               nodename,
+                                               "hostname;",
+					       cassandra_path+"./flush2sstables LogData User;",
+					       cassandra_path+"./alljson "+cassandra_data_path +" "+output_path+"LogData_User."+nodename
+					       );
+        pb.redirectErrorStream(true);
         try{
             Process process = pb.start();
             InputStream inputStream = process.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = null;
             while((line = reader.readLine())!= null) {
-                System.out.println(line);
+                System.out.println(nodename+"  "+line);
             }
             process.waitFor();
         }
